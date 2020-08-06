8197fa0dc94870406aa679618c1b79b5b16dea64
zaki <ahmedzakariya355@gmail.com>
2020-08-06 18:08:28 -0400

    Update

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
-module.exports = {
-	 MongoURL : 'mongodb://localhost:27017/zakdb',
+module.exports   = {
+	// MongoURL : 'mongodb://localhost:27017/dev',
+	MongoURL: ' "mongodb+srv://development-cluster.axeqh.gcp.mongodb.net/dev" --username "ikazaki" --password "Jotario_Kujo"'
+
 	settings : {useNewUrlParser: true, useUnifiedTopology: true}
 
 }
