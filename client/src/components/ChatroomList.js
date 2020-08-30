92dc8dedd42f2f2ad0d028bfbe9a3e75f1027012
MuazAlhaidar <omunam10@gmail.com>
2020-08-30 19:24:16 -0400

    Added add classroom button, DID NOT ADD: remove classroom button

--- client/src/components/ChatroomList.js
+++ client/src/components/ChatroomList.js
@@ -1,11 +1,49 @@
 import React, { useState, useEffect } from "react";
+import { v1 as uuid } from "uuid";
 
 import "../CSS/ChatroomList.css";
 
 function ChatroomList() {
+  const initialChatrooms = [
+    { url: uuid(), name: "Kingdom" },
+    { url: uuid(), name: "Cheeseland" },
+    { url: uuid(), name: "Flownder" },
+    { url: uuid(), name: "Chowder" },
+  ];
+
+  const initialState = JSON.parse(window.localStorage.getItem("chatrooms"));
+
+  const [chatrooms, setChatrooms] = useState(initialState || initialChatrooms);
+
+  useEffect(() => {
+    window.localStorage.setItem("chatrooms", JSON.stringify(chatrooms));
+  });
+
+  const addChatroom = () => {
+    const newName = prompt("Enter Name");
+    if (newName) {
+      const newChatrooms = chatrooms.map((chatroom) => {
+        return chatroom;
+      });
+
+      newChatrooms.push({ url: uuid(), name: newName });
+
+      setChatrooms(newChatrooms);
+    }
+  };
+
   return (
     <div className="ChatroomList">
-      <div className="ChatroomListAddButton">+</div>
+      <div>
+        {chatrooms.map((chatroom) => (
+          <div className="ChatroomList-chatroomcontainer">
+            <div className="ChatroomList-chatroomOption">{chatroom.name}</div>
+          </div>
+        ))}
+      </div>
+      <div className="ChatroomListAddButton" onClick={addChatroom}>
+        +
+      </div>
     </div>
   );
 }
