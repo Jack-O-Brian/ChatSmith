c4181d3ca2b0a3e6477402c52dba0f259166d689
MuazAlhaidar <omunam10@gmail.com>
2020-08-30 18:40:09 -0400

    Creating chatroomlist addbutton css

--- client/src/components/ChatroomList.js
+++ client/src/components/ChatroomList.js
@@ -3,7 +3,11 @@ import React, { useState, useEffect } from "react";
 import "../CSS/ChatroomList.css";
 
 function ChatroomList() {
-  return <div className="ChatroomList"></div>;
+  return (
+    <div className="ChatroomList">
+      <div className="ChatroomListAddButton">+</div>
+    </div>
+  );
 }
 
 export default ChatroomList;
